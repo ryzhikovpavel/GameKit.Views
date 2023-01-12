0110b08e75ac2e8b801234055df79bc173d34878
ryzhikovpavel <ryzhikovpavel@gmail.com>
2023-01-12 19:11:17 +0400

    add package file add safe area component

---
+++ Runtime/Components/SafeAreaRect.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using GameKit.Views.Core;
+using UnityEngine;
+
+namespace GameKit.Views.Components
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class SafeAreaRect : MonoBehaviour
+    {
+        [Flags]
+        private enum Direction
+        {
+            Left = 1,
+            Right = 2,
+            Top = 4,
+            Bottom = 8
+        }
+
+        [SerializeField] private Direction direction;
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = (RectTransform) transform;
+        }
+
+        internal void Sync(Rect safeArea)
+        {
+            var anchorMin = safeArea.position;
+            var anchorMax = safeArea.position + safeArea.size;
+
+            if (direction.HasFlag(Direction.Left))
+            {
+                anchorMin.x /= Screen.width;
+            }
+            else
+            {
+                anchorMin.x = 0;
+            }
+            
+            if (direction.HasFlag(Direction.Right))
+            {
+                anchorMax.x /= Screen.width;
+            }
+            else
+            {
+                anchorMax.x = 1;
+            }
+
+            if (direction.HasFlag(Direction.Top))
+            {
+                anchorMax.y /= Screen.height;
+            }
+            else
+            {
+                anchorMax.y = 1;
+            }
+            
+            if (direction.HasFlag(Direction.Bottom))
+            {
+                anchorMin.y /= Screen.height;
+            }
+            else
+            {
+                anchorMin.y = 0;
+            }
+            
+            _rectTransform.anchorMin = anchorMin;
+            _rectTransform.anchorMax = anchorMax;
+        }
+        
+        private void OnEnable()
+        {
+            Service<ProcessorSafeArea>.Instance.Subscribe(this);
+        }
+
+        private void OnDisable()
+        {
+            Service<ProcessorSafeArea>.Instance.Unsubscribe(this);
+        }
+
+        private void Reset()
+        {
+            if (transform is RectTransform rt)
+            {
+                rt.anchorMax = new Vector2(1, 1);
+                rt.anchorMin = new Vector2(0, 0);
+                rt.offsetMin = new Vector2(0, 0);
+                rt.offsetMax = new Vector2(0, 0);
+            }
+
+            var canvas = GetComponent<Canvas>();
+            if (canvas != null && canvas.isRootCanvas)
+            {
+                Debug.LogError($"{nameof(SafeAreaRect)} dont work with Root Canvas");
+            }
+        }
+    }
+}
---
+++ Runtime/Core/ProcessorSafeArea.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using GameKit.Views.Components;
+using UnityEngine;
+
+namespace GameKit.Views.Core
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class ProcessorSafeArea
+    {
+        private readonly List<SafeAreaRect> items = new List<SafeAreaRect>();
+        private bool ready;
+        private Rect safeArea;
+        
+        public ProcessorSafeArea()
+        {
+            Loop.EventUpdate += OnUpdate;
+        }
+
+        public void Subscribe(SafeAreaRect rect)
+        {
+            items.Add(rect);
+            if (ready)
+                rect.Sync(safeArea);
+        }
+
+        public void Unsubscribe(SafeAreaRect rect)
+        {
+            var i = items.IndexOf(rect);
+            if (i >= 0) items.RemoveAt(i);
+        }
+        
+        private void OnUpdate()
+        {
+            if (safeArea == Screen.safeArea && ready) return;
+            
+            safeArea = Screen.safeArea;
+            if (float.IsNaN(safeArea.x) || float.IsInfinity(safeArea.x) || float.IsNaN(safeArea.y) ||
+                float.IsInfinity(safeArea.y))
+            {
+                ready = false;
+                return;
+            }
+            
+            ready = true;
+            foreach (var item in items)
+            {
+                item.Sync(safeArea);
+            }
+        }
+    }
+}
